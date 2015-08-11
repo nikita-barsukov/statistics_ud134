32bc57926da47dce246e4b3ee60f61eac274db06
Nikita Barsukov <nikita@barsukov.net>
2015-08-11 22:50:16 +0200

    Updated gitignore and writeup as per feedback.

--- script.R
+++ script.R
@@ -1,6 +1,7 @@
 ds = read.csv('stroopdata.csv')
 
 means = sapply(ds, mean)
+medians = sapply(ds, median)
 sds = sapply(ds, sd)
 boxplot(ds)
 title('Distribution of response times\nStroop experiment, seconds')
@@ -11,4 +12,5 @@ par(mfrow=c(2,1))
 qqnorm(ds$Incongruent, pch = 19, col='gray', main='Q-Q plot, incongruent words')
 qqnorm(ds$Congruent, pch = 19, col='gray', main='Q-Q plot, congruent words')
 
-t.test(ds$Congruent, ds$Incongruent, conf.level = 0.99)
+hyp.test = t.test(ds$Congruent, ds$Incongruent, conf.level = 0.99, alternative ='less', paired=TRUE)
+print(hyp.test)
